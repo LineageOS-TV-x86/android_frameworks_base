e071a6f3269d3e447c273ad651438601cf9675a6
saranya <saranya.gopal@intel.com>
2018-04-17 15:52:08 +0530

    GlobalActions: Handle 'sleep' action
    Some Intel platforms do not provide separate
    events for power key press and release. This
    makes it impossible to detect long press of
    power button. So, the solution is to handle
    only short press and add 'sleep' also as an
    option in GlobalActions menu. This patch
    handles 'sleep' option.
    
    Change-Id: Iaae59b324e5ba6eaed9e507fdaa8e5006535716c
    Tracked-On: OAM-56502
    Signed-off-by: saranya <saranya.gopal@intel.com>
    Signed-off-by: Madhusudhan S <madhusudhan.s@intel.com>
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
    
    Conflicts:
    	core/res/res/values-zh-rHK/strings.xml
    	packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialogLite.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialogLite.java
@@ -72,6 +72,7 @@ import android.os.IBinder;
 import android.os.Message;
 import android.os.PowerManager;
 import android.os.RemoteException;
+import android.os.SystemClock;
 import android.os.SystemProperties;
 import android.os.Trace;
 import android.os.UserHandle;
@@ -198,6 +199,7 @@ public class GlobalActionsDialogLite implements DialogInterface.OnDismissListene
     private static final String RESTART_ACTION_KEY_RESTART_BOOTLOADER = "restart_bootloader";
     private static final String RESTART_ACTION_KEY_RESTART_DOWNLOAD = "restart_download";
     private static final String RESTART_ACTION_KEY_RESTART_FASTBOOT = "restart_fastboot";
+    private static final String GLOBAL_ACTION_KEY_SLEEP = "sleep";
 
     // See NotificationManagerService#scheduleDurationReachedLocked
     private static final long TOAST_FADE_TIME = 333;
@@ -696,6 +698,8 @@ public class GlobalActionsDialogLite implements DialogInterface.OnDismissListene
                 addIfShouldShowAction(tempActions, getAssistAction());
             } else if (GLOBAL_ACTION_KEY_RESTART.equals(actionKey)) {
                 addIfShouldShowAction(tempActions, restartAction);
+            } else if (GLOBAL_ACTION_KEY_SLEEP.equals(actionKey)) {
+				addIfShouldShowAction(tempActions, new SleepAction());
             } else if (GLOBAL_ACTION_KEY_SCREENSHOT.equals(actionKey)) {
                 UiModeManager uiModeManager =
                         (UiModeManager) mContext.getSystemService(Context.UI_MODE_SERVICE);
@@ -979,6 +983,37 @@ public class GlobalActionsDialogLite implements DialogInterface.OnDismissListene
             return mHasTelephony;
         }
     }
+    
+    private final class SleepAction extends SinglePressAction implements LongPressAction { 
+        private SleepAction() { 
+            super(R.drawable.ic_restart, R.string.global_action_sleep); 
+        } 
+ 
+        @Override 
+        public boolean onLongPress() { 
+            PowerManager mPowerManager = (PowerManager) 
+                   mContext.getSystemService(Context.POWER_SERVICE); 
+            mPowerManager.goToSleep(SystemClock.uptimeMillis()); 
+            return true; 
+        } 
+ 
+        @Override 
+        public boolean showDuringKeyguard() { 
+            return true; 
+        } 
+ 
+        @Override 
+        public boolean showBeforeProvisioning() { 
+            return true; 
+        } 
+ 
+        @Override 
+        public void onPress() { 
+            PowerManager mPowerManager = (PowerManager) 
+                   mContext.getSystemService(Context.POWER_SERVICE); 
+            mPowerManager.goToSleep(SystemClock.uptimeMillis()); 
+        } 
+    } 
 
     protected int getEmergencyTextColor(Context context) {
         return context.getResources().getColor(
