50397c4e7bea757f6e5ddd21f75b02c7d9cd6d4b
Chih-Wei Huang <cwhuang@linux.org.tw>
2018-10-18 15:47:19 +0800

    pm: ignore restorecon failure (v2)
    On the 9p filesystem, restorecon won't work. It causes apk can't be
    installed. Just ignore the errors to workaround it.
    
    06/10/2023: Ported to Android 14 by HMTheBoy154
    
    Change-Id: I10b592c9de906247837781c20e5d631c441c2df7

--- services/core/java/com/android/server/pm/InstallPackageHelper.java
+++ services/core/java/com/android/server/pm/InstallPackageHelper.java
@@ -1980,8 +1980,6 @@ final class InstallPackageHelper {
 
         if (!onIncremental && !SELinux.restoreconRecursive(afterCodeFile)) {
             Slog.w(TAG, "Failed to restorecon");
-            throw new PrepareFailure(PackageManager.INSTALL_FAILED_MEDIA_UNAVAILABLE,
-                    "Failed to restorecon");
         }
 
         // Reflect the rename internally
